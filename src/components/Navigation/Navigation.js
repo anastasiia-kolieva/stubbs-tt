1e2fc23fa65eafb8f23c1716cde5a24d51562281
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-23 02:39:05 +0300

    added active link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,40 +1,76 @@
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Navigation.module.css';
 
 export default function Navigation() {
+const [activeLink, setActiveLink] = useState("0");
+
+const pressed = event =>{
+  setActiveLink(event.target.id);
+}
+
   return (
 <header >
     <nav className={s.navigation}>
       <ul className={s.navigation__list}>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-        <NavLink exact to="/job-focus" className={s.navigatin__item__link}>Job Focus</NavLink>
+        <li className={activeLink === "1" ? s.navigation__itemActive : s.navigation__item} >
+        <NavLink exact to="/job-focus" 
+        id="1"
+        className={s.navigation__item__link} 
+        onClick={pressed}>Job Focus</NavLink>
         </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-        <NavLink exact to="/soft-skills" className={s.navigatin__item__link}>Soft Skills</NavLink>
+        <li className={activeLink === "2" ? s.navigation__itemActive : s.navigation__item} >
+        <NavLink exact to="/soft-skills" 
+        id="2" 
+        className={s.navigation__item__link} 
+        onClick={pressed}>Soft Skills</NavLink>
         </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-        <NavLink exact to="/technical-skills" className={s.navigatin__item__link}>Technical Skills</NavLink>
+        <li className={activeLink === "3" ? s.navigation__itemActive : s.navigation__item} >
+        <NavLink exact to="/technical-skills" id="3" 
+        className={s.navigation__item__link} 
+        onClick={pressed}>Technical Skills</NavLink>
         </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}> 
-        <a href="#" className={s.navigatin__item__link}>Functional Expertise</a>
+        <li className={activeLink === "4" ? s.navigation__itemActive : s.navigation__item} > 
+        <NavLink exact to="/" 
+        id="4" 
+        className={s.navigation__item__link} 
+        onClick={pressed}>Functional Expertise</NavLink>
         </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-          <a href="#" className={s.navigatin__item__link}>Domain Expertise</a>
+        <li className={activeLink === "5" ? s.navigation__itemActive : s.navigation__item} >
+          <NavLink exact to="/" 
+          id="5" 
+          className={s.navigation__item__link} 
+          onClick={pressed}>Domain Expertise</NavLink>
           </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-          <a href="#" className={s.navigatin__item__link}>Patent Expertice</a>
+        <li className={activeLink === "6" ? s.navigation__itemActive : s.navigation__item} >
+          <NavLink exact to="/" 
+          id="6" 
+          className={s.navigation__item__link} 
+          onClick={pressed}>Patent Expertice</NavLink>
           </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-          <a href="#" className={s.navigatin__item__link}>Personal Expertise</a>
+        <li className={activeLink === "7" ? s.navigation__itemActive : s.navigation__item} >
+          <NavLink exact to="/" 
+          id="7" 
+          className={s.navigation__item__link} 
+          onClick={pressed}>Personal Expertise</NavLink>
           </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-          <a href="#" className={s.navigatin__item__link}>Hard Expertise</a>
+        <li className={activeLink === "8" ? s.navigation__itemActive : s.navigation__item} >
+          <NavLink exact to="/" 
+          id="8" 
+          className={s.navigation__item__link} 
+          onClick={pressed}>Hard Expertise</NavLink>
           </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-          <a href="#" className={s.navigatin__item__link}>Domain Expertise</a>
+        <li className={activeLink === "9" ? s.navigation__itemActive : s.navigation__item} >
+          <NavLink exact to="/" 
+          id="9" 
+          className={s.navigation__item__link} 
+          onClick={pressed}>Domain Expertise</NavLink>
           </li>
-        <li className={s.navigation__item} activeClassName={s.navigatin__item__active}>
-          <a href="#" className={s.navigatin__item__link}>Domain Expertise</a>
+        <li className={activeLink === "10" ? s.navigation__itemActive : s.navigation__item} >
+          <NavLink exact to="/" 
+          id="10" 
+          className={s.navigation__item__link} 
+          onClick={pressed}>Domain Expertise</NavLink>
         </li>
       </ul>
       </nav> 
