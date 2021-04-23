33c81d258181ec0997db2b477c8831dd755bbafe
Anastasiia Kolieva <63116890+anastasiia-kolieva@users.noreply.github.com>
2021-04-23 17:55:34 +0300

    added disabeled styles

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,90 +4,106 @@ import s from './Navigation.module.css';
 
 export default function Navigation() {
 const [activeLink, setActiveLink] = useState("0");
+const [shadowLeft, setShadowLeft] = useState('');
+const [shadowRight, setShadowRight] = useState('');
 
-const itemRef = useRef()
+const itemRef = useRef();
+const listRef = useRef();
 
-const pressed = event =>{
+const handelPress = event =>{
   setActiveLink(event.target.id);
 
-  itemRef.current.scrollIntoView({ block: "end",  behavior: 'smooth' })
+  itemRef.current.scrollIntoView({ block: "end",  behavior: 'smooth' });
+}
+
+const handelScroll = () =>{
+  const scrollX = listRef.current.scrollX;
+  console.log(`onScroll, listRef.current.scrollX: ${scrollX}`)
+
+  if(scrollX === 0){
+    setShadowLeft('shadow-left');
+  } else if(scrollX >= 1860){
+    setShadowRight('shadow-right');
+  }
 }
 
   return (
 <header >
     <nav className={s.navigation}>
-      <ul className={s.navigation__list}>
+      <ul ref={listRef} className={s.navigation__list} onScroll={handelScroll}>
         <li className={activeLink === "1" ? s.navigation__itemActive : s.navigation__item} >
         <NavLink exact to="/job-focus" 
         id="1"
         ref={itemRef}
         className={s.navigation__item__link} 
-        onClick={pressed}>Job Focus</NavLink>
+        onClick={handelPress}>Job Focus</NavLink>
         </li>
         <li className={activeLink === "2" ? s.navigation__itemActive : s.navigation__item} >
         <NavLink exact to="/soft-skills" 
         id="2"
         ref={itemRef} 
         className={s.navigation__item__link} 
-        onClick={pressed}>Soft Skills</NavLink>
+        onClick={handelPress}>Soft Skills</NavLink>
         </li>
         <li className={activeLink === "3" ? s.navigation__itemActive : s.navigation__item} >
         <NavLink exact to="/technical-skills" 
         id="3" 
         ref={itemRef}
         className={s.navigation__item__link} 
-        onClick={pressed}>Technical Skills</NavLink>
+        onClick={handelPress}>Technical Skills</NavLink>
         </li>
         <li className={activeLink === "4" ? s.navigation__itemActive : s.navigation__item} > 
         <NavLink exact to="/" 
         id="4" 
         ref={itemRef}
         className={s.navigation__item__link} 
-        onClick={pressed}>Functional Expertise</NavLink>
+        onClick={handelPress}>Functional Expertise</NavLink>
         </li>
         <li className={activeLink === "5" ? s.navigation__itemActive : s.navigation__item} >
           <NavLink exact to="/" 
           id="5" 
           ref={itemRef}
           className={s.navigation__item__link} 
-          onClick={pressed}>Domain Expertise</NavLink>
+          onClick={handelPress}>Domain Expertise</NavLink>
           </li>
         <li className={activeLink === "6" ? s.navigation__itemActive : s.navigation__item} >
           <NavLink exact to="/" 
           id="6"
           ref={itemRef} 
           className={s.navigation__item__link} 
-          onClick={pressed}>Patent Expertice</NavLink>
+          onClick={handelPress}>Patent Expertice</NavLink>
           </li>
         <li className={activeLink === "7" ? s.navigation__itemActive : s.navigation__item} >
           <NavLink exact to="/" 
           id="7" 
           ref={itemRef}
           className={s.navigation__item__link} 
-          onClick={pressed}>Personal Expertise</NavLink>
+          onClick={handelPress}>Personal Expertise</NavLink>
           </li>
         <li className={activeLink === "8" ? s.navigation__itemActive : s.navigation__item} >
           <NavLink exact to="/" 
           id="8" 
           ref={itemRef}
           className={s.navigation__item__link} 
-          onClick={pressed}>Hard Expertise</NavLink>
+          onClick={handelPress}>Hard Expertise</NavLink>
           </li>
         <li className={activeLink === "9" ? s.navigation__itemActive : s.navigation__item} >
           <NavLink exact to="/" 
           id="9" 
           ref={itemRef}
           className={s.navigation__item__link} 
-          onClick={pressed}>Domain Expertise</NavLink>
+          onClick={handelPress}>Domain Expertise</NavLink>
           </li>
-        <li className={activeLink === "10" ? s.navigation__itemActive : s.navigation__item} >
+        <li className={s.navigation__disabled} >
           <NavLink exact to="/" 
           id="10" 
           ref={itemRef}
-          className={s.navigation__item__link} 
-          onClick={pressed}>Domain Expertise</NavLink>
+          className={s.navigation__item__linkDisabled } 
+          onClick={handelPress}>Domain Expertise</NavLink>
         </li>
       </ul>
+      <div className={`shadow ${shadowLeft}`} ></div>
+      <div className={`shadow ${shadowRight}`} ></div>
       </nav> 
 </header>
 )
